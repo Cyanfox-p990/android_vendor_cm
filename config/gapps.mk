9da4ae9f3d7052b624376640e72fd92759e073e4
MNT <m.zilius@gmail.com>
2013-12-02 00:41:33 +0200

    Bring back Gapps core only

--- config/gapps.mk
+++ config/gapps.mk
@@ -2,30 +2,10 @@
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/app/app/LatinImeGoogle.apk:system/app/LatinImeGoogle.apk \
     vendor/cm/prebuilt/common/app/app/GoogleHome.apk:system/app/GoogleHome.apk \
-    vendor/cm/prebuilt/common/app/app/Hangouts.apk:system/app/Hangouts.apk \
-    vendor/cm/prebuilt/common/app/app/CalendarGoogle.apk:system/app/CalendarGoogle.apk \
-    vendor/cm/prebuilt/common/app/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
-    vendor/cm/prebuilt/common/app/app/GenieWidget.apk:system/app/GenieWidget.apk \
-    vendor/cm/prebuilt/common/app/app/Gmail2.apk:system/app/Gmail2.apk \
-    vendor/cm/prebuilt/common/app/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
-    vendor/cm/prebuilt/common/app/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
-    vendor/cm/prebuilt/common/app/app/YouTube.apk:system/app/YouTube.apk \
-    vendor/cm/prebuilt/common/app/app/Maps.apk:system/app/Maps.apk \
-    vendor/cm/prebuilt/common/app/app/MediaUploader.apk:system/app/MediaUploader.apk \
-    vendor/cm/prebuilt/common/app/app/PlusOne.apk:system/app/PlusOne.apk
+    vendor/cm/prebuilt/common/app/app/Hangouts.apk:system/app/Hangouts.apk
 
 # priv-app
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/app/priv-app/GoogleBackupTransport.apk:system/priv-app/GoogleBackupTransport.apk \
-    vendor/cm/prebuilt/common/app/priv-app/GoogleFeedback.apk:system/priv-app/GoogleFeedback.apk \
-    vendor/cm/prebuilt/common/app/priv-app/GoogleLoginService.apk:system/priv-app/GoogleLoginService.apk \
-    vendor/cm/prebuilt/common/app/priv-app/GooglePartnerSetup.apk:system/priv-app/GooglePartnerSetup.apk \
-    vendor/cm/prebuilt/common/app/priv-app/GoogleServicesFramework.apk:system/priv-app/GoogleServicesFramework.apk \
-    vendor/cm/prebuilt/common/app/priv-app/OneTimeInitializer.apk:system/priv-app/OneTimeInitializer.apk \
-    vendor/cm/prebuilt/common/app/priv-app/Phonesky.apk:system/priv-app/Phonesky.apk \
-    vendor/cm/prebuilt/common/app/priv-app/PrebuiltGmsCore.apk:system/priv-app/PrebuiltGmsCore.apk \
-    vendor/cm/prebuilt/common/app/priv-app/SetupWizard.apk:system/priv-app/SetupWizard.apk \
-    vendor/cm/prebuilt/common/app/priv-app/CalendarProvider.apk:system/priv-app/CalendarProvider.apk \
     vendor/cm/prebuilt/common/app/priv-app/Velvet.apk:system/priv-app/Velvet.apk
 
 # lib
@@ -33,47 +13,7 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/app/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
     vendor/cm/prebuilt/common/app/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
     vendor/cm/prebuilt/common/app/lib/libgoogle_recognizer_jni_l.so:system/lib/libgoogle_recognizer_jni_l.so \
-    vendor/cm/prebuilt/common/app/lib/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so \
-    vendor/cm/prebuilt/common/app/lib/libAppDataSearch.so:system/lib/libAppDataSearch.so \
-    vendor/cm/prebuilt/common/app/lib/libgames_rtmp_jni.so:system/lib/libgames_rtmp_jni.so \
-    vendor/cm/prebuilt/common/app/lib/libfacetracker.so:system/lib/libfacetracker.so \
-    vendor/cm/prebuilt/common/app/lib/libfilterframework_jni.so:system/lib/libfilterframework_jni.so \
-    vendor/cm/prebuilt/common/app/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so \
-    vendor/cm/prebuilt/common/app/lib/libfrsdk.so:system/lib/libfrsdk.so \
-    vendor/cm/prebuilt/common/app/lib/libjni_t13n_shared_engine.so:system/lib/libjni_t13n_shared_engine.so \
-    vendor/cm/prebuilt/common/app/lib/libmoviemaker-jni.so:system/lib/libmoviemaker-jni.so \
-    vendor/cm/prebuilt/common/app/lib/libnetjni.so:system/lib/libnetjni.so \
-    vendor/cm/prebuilt/common/app/lib/libpatts_engine_jni_api.so:system/lib/libpatts_engine_jni_api.so \
-    vendor/cm/prebuilt/common/app/lib/libplus_jni_v8.so:system/lib/libplus_jni_v8.so \
-    vendor/cm/prebuilt/common/app/lib/librs.antblur.so:system/lib/librs.antblur.so \
-    vendor/cm/prebuilt/common/app/lib/librs.antblur_constant.so:system/lib/librs.antblur_constant.so \
-    vendor/cm/prebuilt/common/app/lib/librs.antblur_drama.so:system/lib/librs.antblur_drama.so \
-    vendor/cm/prebuilt/common/app/lib/librs.drama.so:system/lib/librs.drama.so.so \
-    vendor/cm/prebuilt/common/app/lib/librs.film_base.so:system/lib/librs.film_base.so \
-    vendor/cm/prebuilt/common/app/lib/librs.fixedframe.so:system/lib/librs.fixedframe.so \
-    vendor/cm/prebuilt/common/app/lib/librs.grey.so:system/lib/librs.grey.so.so \
-    vendor/cm/prebuilt/common/app/lib/librs.image_wrapper.so:system/lib/librs.image_wrapper.so \
-    vendor/cm/prebuilt/common/app/lib/librs.retrolux.so:system/lib/librs.retrolux.so \
-    vendor/cm/prebuilt/common/app/lib/librsjni.so:system/lib/librsjni.so \
-    vendor/cm/prebuilt/common/app/lib/libRSSupport.so:system/lib/libRSSupport.so \
-    vendor/cm/prebuilt/common/app/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
-    vendor/cm/prebuilt/common/app/lib/libwebp_android.so:system/lib/libwebp_android.so \
-    vendor/cm/prebuilt/common/app/lib/libwebrtc_audio_coding.so:system/lib/libwebrtc_audio_coding.so \
-    vendor/cm/prebuilt/common/app/lib/libwebrtc_audio_preprocessing.so:system/lib/libwebrtc_audio_preprocessing.so
-
-# framework
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/app/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
-    vendor/cm/prebuilt/common/app/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
-    vendor/cm/prebuilt/common/app/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar
-
-# etc
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/app/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
-    vendor/cm/prebuilt/common/app/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
-    vendor/cm/prebuilt/common/app/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
-    vendor/cm/prebuilt/common/app/etc/permissions/features.xml:system/etc/permissions/features.xml \
-    vendor/cm/prebuilt/common/app/etc/preferred-apps/google.xml:system/etc/preferred-apps/google.xml
+    vendor/cm/prebuilt/common/app/lib/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so
 
 # usr
 PRODUCT_COPY_FILES += \
